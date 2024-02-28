c5bd49f3e5b624892fdec2c546d99e016e42d395
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-02-28 11:35:01 +0200

    Added infrastructure changes

---
+++ src/main/java/com/agencyamazon/AgencyAmazonApplication.java
@@ -0,0 +1,13 @@
+package com.agencyamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyAmazonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgencyAmazonApplication.class, args);
+    }
+
+}
